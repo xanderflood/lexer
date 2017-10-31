24cd0a30411c45d0d9b1d93e63ecb3de3ab04a4c
xanderflood <xander.t.flood@gmail.com>
2017-10-30 20:08:41 -0400

    Threw out some blnak lines in syntax.c

--- mod/parse/syntax.c
+++ mod/parse/syntax.c
@@ -10,33 +10,22 @@
 
 const expression_format expr_type_fmts[num_expr_types] = {
   INV_EXP_FMT,   // IND_EXPR      indeterminate
-
   ROOT_EXPR_FMT, // ROOT_EXPR     placeholder at the top of a statement's expr tree
-
-  // noun literals (leaves)
   LEAF_EXPR_FMT, // LIT_NUM_EXPR  numeric literal
   LEAF_EXPR_FMT, // LIT_STR_EXPR  string literal
   LEAF_EXPR_FMT, // SYMBOL_EXPR   string data
-
-  // noun expressions
   VAR_DECL_EXPR_FMT, // DECL_EXPR      string data (similar to SYMBOL but declares a )
   ARRAY_EXPR_FMT,    // ARRAY_EXPR     arbitrary list of expressions
   OBJECT_EXPR_FMT,   // OBJECT_EXPR    alternates between symbols and RVALUE types
   FCN_DEFN_EXPR_FMT, // FUNCTION_EXPR  a function definition
-
-  // operations (internal nodes)
   PAREN_EXPR_FMT,       // PAREN_EXPR         (a)
   INFIX_BINOP_EXPR_FMT, // ACCESS_EXPR        a.b
   COMP_ACCESS_EXPR_FMT, // COMP_ACCESS_EXPR   a[b]
   NEW_ARGS_EXPR_FMT,    // NEW_ARGS_EXPR      new a (b, ...)
   FCN_CALL_EXPR_FMT,    // CALL_EXPR          a(...)
-
   PREFIX_UNOP_EXPR_FMT, // NEW_EXPR           new a
-
   POSTFIX_UNOP_EXPR_FMT, // POST_INCR_EXPR     a ++
   POSTFIX_UNOP_EXPR_FMT, // POST_DECR_EXPR     a --
-
-  // prefix unary operations
   PREFIX_UNOP_EXPR_FMT, // NOT_EXPR           ! a
   PREFIX_UNOP_EXPR_FMT, // BITNOT_EXPR        ~ a
   PREFIX_UNOP_EXPR_FMT, // POS_EXPR           + a
@@ -46,44 +35,31 @@ const expression_format expr_type_fmts[num_expr_types] = {
   PREFIX_UNOP_EXPR_FMT, // TYPEOF_EXPR        typdeof a
   PREFIX_UNOP_EXPR_FMT, // VOID_EXPR          void a
   PREFIX_UNOP_EXPR_FMT, // DELETE_EXPR        delete a
-
   INFIX_BINOP_EXPR_FMT, // EXP_EXPR           a ** b
-
   INFIX_BINOP_EXPR_FMT, // TIMES_EXPR         a * b
   INFIX_BINOP_EXPR_FMT, // QUOT_EXPR          a / b
   INFIX_BINOP_EXPR_FMT, // MOD_EXPR           a % b
-
   INFIX_BINOP_EXPR_FMT, // PLUS_EXPR          a + b
   INFIX_BINOP_EXPR_FMT, // MINUS_EXPR         a - b
-
   INFIX_BINOP_EXPR_FMT, // BSLEFT_EXPR        a << b
   INFIX_BINOP_EXPR_FMT, // BSRIGHT_EXPR       a >> b
   INFIX_BINOP_EXPR_FMT, // UBSRIGHT_EXPR      a >>> b
-
   INFIX_BINOP_EXPR_FMT, // LT_EXPR            a < b
   INFIX_BINOP_EXPR_FMT, // GT_EXPR            a > b
   INFIX_BINOP_EXPR_FMT, // LET_EXPR           a <= b
   INFIX_BINOP_EXPR_FMT, // GET_EXPR           a >= b
   INFIX_BINOP_EXPR_FMT, // IN_EXPR            a in b
   INFIX_BINOP_EXPR_FMT, // INSTANCEOF_EXPR    a instanceof b
-
   INFIX_BINOP_EXPR_FMT, // EQ_EXPR            a == b
   INFIX_BINOP_EXPR_FMT, // INEQ_EXPR          a != b
   INFIX_BINOP_EXPR_FMT, // STRICT_EQ_EXPR     a === b
   INFIX_BINOP_EXPR_FMT, // STRICT_INEQ_EXPR   a !== b
-
   INFIX_BINOP_EXPR_FMT, // BAND_EXPR          a & b
-
   INFIX_BINOP_EXPR_FMT, // BXOR_EXPR          a ^ b
-
   INFIX_BINOP_EXPR_FMT, // BOR_EXPR           a | b
-
   INFIX_BINOP_EXPR_FMT, // LAND_EXPR          a && b
-
   INFIX_BINOP_EXPR_FMT, // LOR_EXPR           a || b
-
   CONF_TRIOP_EXPR_FMT,  // COND_EXPR          a ? b : c
-
   INFIX_BINOP_EXPR_FMT, // ASSIGN_EXPR           a = b
   INFIX_BINOP_EXPR_FMT, // ASSIGN_PLUS_EXPR      a += b
   INFIX_BINOP_EXPR_FMT, // ASSIGN_TIMES_EXPR     a *= b
@@ -96,12 +72,9 @@ const expression_format expr_type_fmts[num_expr_types] = {
   INFIX_BINOP_EXPR_FMT, // ASSIGN_BAND_EXPR      a &= b
   INFIX_BINOP_EXPR_FMT, // ASSIGN_BXOR_EXPR      a ^= b
   INFIX_BINOP_EXPR_FMT, // ASSIGN_BOR_EXPR       a |= b
-
   PREFIX_UNOP_EXPR_FMT, // YIELD_EXPR            yield a
   PREFIX_UNOP_EXPR_FMT, // YIELD_STAR_EXPR       yield *a
-
   PREFIX_UNOP_EXPR_FMT, // SPREAD_EXPR           ... a
-
   INFIX_BINOP_EXPR_FMT  // COMMA_EXPR            a, b (valuates to b)
 };
 
@@ -170,40 +143,31 @@ void init_state(JS_EXPR *expr) {
 //       AST building process.
 const bool expr_type_after_the_fact[num_expression_formats] = {
   false, // LEAF_EXPR_FMT
-
   false, // ROOT_EXPR_FMT
-
   false, // VAR_DECL_EXPR_FMT
   false, // ARRAY_EXPR_FMT
   false, // OBJECT_EXPR_FMT
   false, // FCN_DEFN_EXPR_FMT
-
   false, // PREFIX_UNOP_EXPR_FMT
   true,  // POSTFIX_UNOP_EXPR_FMT
   true,  // INFIX_BINOP_EXPR_FMT
-
   false, // PAREN_EXPR_FMT
   true,  // COMP_ACCESS_EXPR_FMT
   false, // NEW_ARGS_EXPR_FMT
   true,  // FCN_CALL_EXPR_FMT
-
   true   // CONF_TRIOP_EXPR_FMT
 };
 
 const expression_precedence opr_prec_info[num_expr_types] = {
   INVALID_EXPR_PREC, // IND_EXPR
-
   LEAF_EXPR_PREC, // ROOT_EXPR
-
   LEAF_EXPR_FMT, // LIT_NUM_EXPR  numeric literal
   LEAF_EXPR_FMT, // LIT_STR_EXPR  string literal
   LEAF_EXPR_FMT, // SYMBOL_EXPR   string data
-
   CONTAINER_EXPR_PREC, // DECL_EXPR      string data (similar to SYMBOL but declares a )
   CONTAINER_EXPR_PREC, // ARRAY_EXPR     arbitrary list of expressions
   CONTAINER_EXPR_PREC, // OBJECT_EXPR    alternates between symbols and RVALUE types
   CONTAINER_EXPR_PREC, // FUNCTION_EXPR  a function definition
-
   20, // PAREN_EXPR         (a)
   19, // ACCESS_EXPR        a.b
   19, // COMP_ACCESS_EXPR   a[b]
