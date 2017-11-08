bf2b30d754dbba65bd8bd45a7b1de79bf190ccad
Xander Flood <xander.t.flood@gmail.com>
2017-11-08 11:32:47 -0500

    More-or-less finished read_statement logic

--- mod/parse/read.c
+++ mod/parse/read.c
@@ -27,60 +27,63 @@ int read_statement(JS_STMT *stmt) {
     return ret;
 
   // TODO: if first token is KEY_TOK || PNC_TOK('{'),
-  // consider other statement types
+  //       consider other statement types
   // otherwise:
 
   stmt->type = EXPR_STMT;
   current = stmt->expressions->data;
 
+  //
+  // TODO: `current` is always EITHER expectant or non-terminal
+  // No type other than a ROOT_EXPR is ever terminal and expectant at once
+  //
   do {
-    // TODO: After a node accepts a signal token, it's first
-    //   child is marked invisible. (In order words, we can't
-    //   descend past it until it accepts a new child.)
+    // There are only a handful of tokens used as signals, and they are all
+    // single-character: ";,]):". None of them are nouns, and only the comma
+    // is ever a verb.
 
-    //
-    // NOTE: here, "noun" means inserted as a child, and "verb" means
-    // claiming a child from the syntax tree
-    //
-    // TODO: `current` is always EITHER expectant or non-terminal
-    // No type other than a ROOT_EXPR is ever terminal and expectant at once
-    //
-    // TODO: This doesn't properly handle NTERM_CS_EXST, which is only used by ROOT_EXPR
-    //       It's is an expectant state, so it'll never reach the else if it is passed a semicolon
-    if (EXPECTANT_EXPR_STATE(current)) {
-      // If it's expectant, then try to interpret `tok` as a noun.
-      et = interpret_operator_token_as_noun(&tok);
-
-      if (et == IND_EXPR) {
-        return -1; // raise UNEXPECTED_????
-      } else {
-        new = init_expression(et);
-        new->data = strdup(tok.s);
-
-        add_child_expression(current, new);
-
-        current = new;
-      }
-    } else {
-
-      // The only overlap between the signal tokens ";,]):" and
-      // the verb tokens is the comma. It should only be used as
-      // a verb if it is not accepted as a token, so we try
-      // update_state *first*.
-
+    bool signalled = false;
+    if (EXPECTING_SIGNAL_STATE(current)) {
       if (! update_state(current, &tok)) {
+        signalled = true;
         // TODO: state was updated, do we need to do anything?
-      } else {
-        et = interpret_operator_token_as_verb(&tok);
-        // seek_downwards
-        // add_postfix_child_expression
       }
-      // TODO: need to implement child visibility in expression state
+    }
 
-      // (2) If it's not expectant, then it's non-terminal, and waiting on signal tokens.
-      // (3) HOWEVER if it is non-expectant, non-terminal AND has
-      //  one visible child and the signal token is rejected, we
-      //  should try to re-interpret it as a verb operating on that child
+    if (! signalled) {
+      if (EXPECTANT_EXPR_STATE(current)) {
+        // If it's expectant, then try to interpret `tok` as a noun.
+        et = interpret_operator_token_as_noun(&tok);
+
+        if (et == IND_EXPR) {
+          return -1; // raise UNEXPECTED_????
+        } else {
+          new = init_expression(et);
+          new->data = strdup(tok.s);
+
+          add_child_expression(current, new);
+
+          current = new;
+        }
+      } else if (VIS_CHILD_EXPR_STATE(current)) {
+        et = interpret_operator_token_as_verb(&tok);
+
+        if (et == IND_EXPR) {
+          return -1; // raise UNEXPECTED_????
+        } else {
+          new = init_expression(et);
+          new->data = strdup(tok.s);
+
+          seek_downwards(&current, new);
+
+          add_child_expression(current, new);
+
+          current = new;
+        }
+      } else {
+        return -1; // raise UNRECOGNIZED_TOKEN;
+                   // or    EXPECTED_SEMICOLON; ??
+      }
     }
 
     if (seek_upwards(&current))
